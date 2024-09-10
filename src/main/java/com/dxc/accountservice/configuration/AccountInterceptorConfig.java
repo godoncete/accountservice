f5f8b6faa8164116509e0bc1f23c4a260233e107
Oscar <you@example.com>
2024-09-10 15:48:16 +0200

    break filter

---
+++ src/main/java/com/dxc/accountservice/configuration/AccountInterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.dxc.accountservice.configuration;
+
+import com.dxc.accountservice.interceptors.AccountInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class AccountInterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AccountInterceptor accountInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accountInterceptor).addPathPatterns("/account/**");
+    }
+}
---
+++ src/main/java/com/dxc/accountservice/filters/AccountFilter.java
@@ -0,0 +1,41 @@
+package com.dxc.accountservice.filters;
+
+import org.springframework.stereotype.Component;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.LogRecord;
+
+@Component
+public class AccountFilter implements Filter {
+
+
+    private List<String> ips;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        ips = List.of("127.0.0.1", "0.0.0.0", "192.168.0.1","192.168.240.48");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//            logger.info("Filtrando 2..."+ servletRequest.getLocalAddr()+"::"+servletRequest.getRemoteHost());
+                String ip_ = servletRequest.getLocalAddr();
+                boolean isOk = ips.stream().filter(ip -> ip_.equals(ip)).findFirst().isPresent();
+                if (!isOk) {
+                    ((HttpServletResponse) servletResponse).setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    return;
+                }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
