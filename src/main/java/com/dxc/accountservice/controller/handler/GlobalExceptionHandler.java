29e5809b694572505d30da40f05c19b927b8ff23
micuenta33 <mohammedelyousfi@gmial.com>
2024-09-13 15:36:06 +0200

    saving changes

--- src/main/java/com/dxc/accountservice/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dxc/accountservice/controller/handler/GlobalExceptionHandler.java
@@ -15,36 +15,36 @@ import java.util.HashMap;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> handleP(ProductNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleU(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-     @ExceptionHandler(UsuarioNotFoundException.class)
-    public ResponseEntity<String> handleU(UsuarioNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
-        var errors = new HashMap<String, String>();
-
-        e.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String message = error.getDefaultMessage();
-            errors.put(fieldName, message);
-        });
-        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(new ErrorResponse(errors));
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-
-        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-
-    }
+//    @ExceptionHandler(ProductNotFoundException.class)
+//    public ResponseEntity<String> handleP(ProductNotFoundException e) {
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+//    }
+//     @ExceptionHandler(IllegalArgumentException.class)
+//    public ResponseEntity<String> handleU(IllegalArgumentException e) {
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
+//     @ExceptionHandler(UsuarioNotFoundException.class)
+//    public ResponseEntity<String> handleU(UsuarioNotFoundException e) {
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+//    }
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+//        var errors = new HashMap<String, String>();
+//
+//        e.getBindingResult().getAllErrors().forEach(error -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String message = error.getDefaultMessage();
+//            errors.put(fieldName, message);
+//        });
+//        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(new ErrorResponse(errors));
+//    }
+//
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+//
+//        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+//
+//    }
 }
