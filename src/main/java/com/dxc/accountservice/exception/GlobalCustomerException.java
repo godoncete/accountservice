f2176b814efa4ac37a9e8e35f48dc442fcfb61d4
pablochia <pablochia2021@outlook.es>
2024-09-13 14:07:55 +0200

    pabloChia

---
+++ src/main/java/com/dxc/accountservice/exception/GlobalCustomerException.java
@@ -0,0 +1,9 @@
+package com.dxc.accountservice.exception;
+
+public class GlobalCustomerException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    public GlobalCustomerException(String message) {
+        super(message);
+    }
+}
